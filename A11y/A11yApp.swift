b2e1887e16b3f77eb21f552cbed2f4cf9f308cda
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-02 09:27:04 -0500

    Initial Commit

---
+++ A11y/A11yApp.swift
@@ -0,0 +1,17 @@
+//
+//  A11yApp.swift
+//  A11y
+//
+//  Created by Keenan Chiasson on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct A11yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
