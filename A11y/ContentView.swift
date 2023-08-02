f808f9857ab29f999d832968a95ccb505810b6e4
keenan-chiasson <keenan.chiasson@acstexas.com>
2023-08-02 10:33:08 -0500

    hide and group accessibility data

--- A11y/ContentView.swift
+++ A11y/ContentView.swift
@@ -8,32 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks"
-    ]
-    
-    @State private var selectedPicture = Int.random(in: 0...3)
-    
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .accessibilityLabel(labels[selectedPicture])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
-            .onTapGesture {
-                selectedPicture = Int.random(in: 0...3)
-            }
+        VStack {
+            Text("Your score is")
+            Text("1000")
+                .font(.title)
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Your score is 1000")
     }
 }
 
